314e8576b13a57206c3842fc63228787024e5473
AliNeamehh <alineamehh@gmail.com>
2024-11-09 08:57:41 +0200

    php<>adding addUser endpoint

---
+++ api/addUser.php
@@ -0,0 +1,14 @@
+<?php
+  include 'db.php';
+
+    $data = json_decode(file_get_contents("php://input"), true);
+    $name=$data['name'];
+
+    $query=$connection->prepare("insert into users(name)values(?)");
+    $query->bind_param("s",$name);
+    $query->execute();
+
+    echo json_encode(["message"=>"User added sucessfully","id"=>$connection->insert_id]);
+
+
+?>
